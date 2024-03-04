e69c1c4bea19d036dc51bd0fcf78cc7ca57509a1
Joseph Furnari <joseph.furnari@s.capregboces.org>
2024-03-04 10:07:46 -0500

    Ready for grading

---
+++ Exercise8/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Exercise8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number:");
+            int maxNumber = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < maxNumber; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(maxNumber);
+
+            Console.WriteLine("Enter number:");
+            int number = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i < number)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
